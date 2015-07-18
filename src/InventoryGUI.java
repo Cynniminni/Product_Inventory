5f5f965631458ade59249359060f03f27fc368d0
Aitalas <radicalcyn@yahoo.com>
2015-07-18 16:01:03 -0700

    Adjusted GUI, all components in place

--- src/InventoryGUI.java
+++ src/InventoryGUI.java
@@ -1,6 +1,9 @@
 import java.awt.Button;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,6 +16,7 @@ public class InventoryGUI {
 	public JFrame frame;
 	
 	public JPanel containerPanel;
+	public JPanel invenPanel;
 	public JPanel actionPanel;
 	public JPanel searchPanel;
 	public JPanel managePanel;
@@ -33,13 +37,14 @@ public class InventoryGUI {
 		
 	public InventoryGUI() {
 		frame = new JFrame();
-		frame.setSize(800, 500);
+		frame.setSize(700, 500);
 		frame.setTitle(title);
+		//frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 				
 		invenTable = new JTextArea();
-		invenTable.setSize(480, 500);
+		invenTable.setSize(470, 500);
 		invenTable.setEditable(false);
 		invenTable.setEnabled(true);
 		invenTable.setLineWrap(true);
@@ -50,7 +55,7 @@ public class InventoryGUI {
 		searchHeader.setFont(new Font("Monospaced", Font.PLAIN, 12));
 		
 		manageHeader = new JLabel("Manage Products");
-		manageHeader.setFont(new Font("Monospaced", Font.PLAIN, 12));
+		manageHeader.setFont(new Font("Monospaced", Font.PLAIN, 12));		
 		
 		searchBar = new JTextField();
 		
@@ -62,25 +67,37 @@ public class InventoryGUI {
 		
 		searchPanel = new JPanel();
 		searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.X_AXIS));
+		searchPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 		searchPanel.add(searchBar);
 		searchPanel.add(searchProduct);
-		searchPanel.add(refreshInven);
+		searchPanel.add(refreshInven);				
 		
 		managePanel = new JPanel();
 		managePanel.setLayout(new BoxLayout(managePanel, BoxLayout.X_AXIS));
+		managePanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
 		managePanel.add(addProduct);
 		managePanel.add(editProduct);
 		managePanel.add(removeProduct);
 		
 		actionPanel = new JPanel();
 		actionPanel.setLayout(new BoxLayout(actionPanel, BoxLayout.Y_AXIS));
+		actionPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);		
 		actionPanel.add(searchHeader);
+		actionPanel.add(Box.createRigidArea(new Dimension(0,5)));
 		actionPanel.add(searchPanel);
 		actionPanel.add(manageHeader);
+		actionPanel.add(Box.createRigidArea(new Dimension(0,5)));
 		actionPanel.add(managePanel);
+		actionPanel.add(Box.createRigidArea(new Dimension(0, 400)));
+		
+		invenPanel = new JPanel();
+		invenPanel.setLayout(new GridLayout(1,1));
+		invenPanel.add(invenTable);
 		
 		containerPanel = new JPanel();//has FlowLayout by default
-		containerPanel.add(invenTable);
+		containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.X_AXIS));
+		containerPanel.add(invenPanel);		
+		containerPanel.add(Box.createRigidArea(new Dimension(10,0)));
 		containerPanel.add(actionPanel);
 		
 		frame.add(containerPanel);
